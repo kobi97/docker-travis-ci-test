555b29098c8cda25422611414925ecd01c9d8ef9
kobi97 <kobi97@ridi.com>
2017-12-28 15:53:59 +0900

    add phpunit test

---
+++ tests/Binlog/BinlogCollectorTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Binlog\Tests;
+
+use Binlog\Collector\Model\BinlogHistoryModel;
+use PHPUnit\Framework\TestCase;
+
+class BinlogCollectorTest extends TestCase
+{
+  public function testResult()
+  {
+      $this->assertTrue(true);
+  }
+}
